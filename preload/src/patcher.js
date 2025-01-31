cb26e4d1738713ce75c13707912dfc4fb423b087
Robert Delaney <Doggybootsy@gmail.com>
2025-01-30 19:44:55 -0800

    Initial

--- preload/src/patcher.js
+++ preload/src/patcher.js
@@ -2,6 +2,29 @@ import {webFrame} from "electron";
 
 /* global window:false */
 
+function pollyFillDefault(module) {
+    if ("default" in module.exports) return;
+
+    function ensureProperty(key, value) {
+        if (key in module.exports) return;
+        
+        Object.defineProperty(module.exports, key, {value});
+    }
+
+    if ("Z" in module.exports || "ZP" in module.exports) {
+        Object.defineProperty(module.exports, "default", {
+            get: () => module.exports.Z || module.exports.ZP,
+            set: (v) => {
+                if ("Z" in module.exports) module.exports.Z = v;
+                if ("ZP" in module.exports) module.exports.ZP = v;
+            }
+        });
+
+        ensureProperty("__esModule", true);
+        ensureProperty(Symbol.toStringTag, "Module");
+    }
+  }
+
 export default function () {
     const patcher = function () {
         const chunkName = "webpackChunkdiscord_app";
@@ -53,6 +76,18 @@ export default function () {
                             }
                         }
                     };
+
+                    require.c = new Proxy(require.c, {
+                        get() {
+                            const module = Reflect.get(...arguments);
+
+                            if (typeof module?.exports === "object") {
+                                pollyFillDefault(module);
+                            }
+
+                            return module;
+                        }
+                    });
                 }]);
             });
         }
